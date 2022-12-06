45bedc8dfb2bedfebc4c4b6ebe8eb5df317706bd
copiter <99804262+copiter@users.noreply.github.com>
2022-12-06 21:49:07 +0900

    [feature] 수정기능

--- selection-task/src/Todo.jsx
+++ selection-task/src/Todo.jsx
@@ -1,14 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import api from './api';
 
 const AUTHORIZATION = `Bearer ${localStorage.getItem("access_token")}`;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > * {
+    margin: 5px;
+  }
+`;
+const Completed = styled.span`
+  display: inline-block;
+  margin-right: 10px;
+  font-weight: 700;
+`;
+const Todoitem = styled.span`
+  display: inline-block;
+  min-width: 100px;
+`;
+
 function Todo() {
   const navigate = useNavigate();
   const [todos, setTodos] = useState([]);
-  const [rerender, setRerender] = useState(false);
   const [inputValue, setInputValue] = useState('');
+  const [updateValue, setUpdateValue] = useState('');
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [updateIdx, setUpdateIdx] = useState(null);
 
   useEffect(() => {
     fetch(api.getTodos(), {
@@ -25,13 +46,13 @@ function Todo() {
       });
   }, []);
 
-  // useEffect(() => {
-  //   const checkJwt = localStorage.getItem("access_token");
+  useEffect(() => {
+    const checkJwt = localStorage.getItem("access_token");
 
-  //   if (checkJwt === null) {
-  //     navigate("/");
-  //   }
-  // }, [navigate]);
+    if (checkJwt === null) {
+      navigate("/");
+    }
+  }, [navigate]);
 
   function handleInput(e) {
     setInputValue(e.target.value);
@@ -47,17 +68,19 @@ function Todo() {
       body: JSON.stringify({todo: inputValue})
     })
       .then((res) => {
-        console.log(res);
         if (res.status === 201) {
           return res.json();
         } 
       })
       .then((data) => {
-        setTodos([...todos, data])
+        setTodos([...todos, data]);
+        setInputValue('');
       });
   }
 
   function handleDeleteTodo(serverId, arrayId) {
+    setIsUpdate(false);
+    
     fetch(api.deleteTodo(serverId), {
       method: "DELETE",
       headers: {
@@ -66,34 +89,44 @@ function Todo() {
     }).then((res) => {
       console.log(res);
       if (res.status === 204) {
-        todos.splice(arrayId, 1);
-        setTodos(todos.filter((item,idx) => idx !== arrayId));
+        setTodos(todos.filter((item, idx) => idx !== arrayId));
+        alert("삭제 되었습니다")
       } else {
         alert("삭제 되지 않았습니다.");
       }
     });
-      
   }
 
+  function handleUpdateValue(e, id) {
+    setUpdateValue(e.target.value)
+  }
+  
+  function handleUpdateTodo(serverId, arrayId, isCompleted) {
+    fetch(api.updateTodo(serverId), {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: AUTHORIZATION,
+      },
+      body: JSON.stringify({
+        todo: updateValue.length === 0 ? todos[arrayId].todo : updateValue,
+        isCompleted: isCompleted,
+      }),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        setTodos(todos.map((item, idx) => (idx === arrayId ? data : item)));
+        setIsUpdate(false);
+        setUpdateValue('')
+      });
+  }
 
   return (
-    <>
-      <ul>
-        {todos.map((item, idx) => (
-          <li key={idx}>
-            <input
-              type="checkbox"
-              checked={item.isCompleted}
-              // onChange={handleUpdateTodo}
-            />
-            <span>{item.todo}</span>
-            <button>수정</button>
-            <button>제출</button>
-            <button>수정취소</button>
-            <button onClick={() => handleDeleteTodo(item.id, idx)}>삭제</button>
-          </li>
-        ))}
-      </ul>
+    <Container>
       <div>
         <label htmlfor="create">추가</label>
         <input
@@ -109,7 +142,55 @@ function Todo() {
           추가
         </button>
       </div>
-    </>
+      <ol>
+        {todos.map((item, idx) => (
+          <li key={idx}>
+            <input
+              type="checkbox"
+              checked={item.isCompleted}
+              onChange={(e) =>
+                handleUpdateTodo(item.id, idx, e.target.checked)
+              }
+            />
+            <Completed>{item.isCompleted ? "완료" : "미완"}</Completed>
+            {isUpdate && idx === updateIdx ? (
+              <>
+                <input
+                  type="text"
+                  name="create"
+                  value={updateValue}
+                  onChange={(e) => handleUpdateValue(e, item.id)}
+                />
+                <button
+                  onClick={() =>
+                    handleUpdateTodo(item.id, idx, item.isCompleted)
+                  }
+                >
+                  제출
+                </button>
+                <button onClick={() => setIsUpdate(false)}>수정취소</button>
+              </>
+            ) : (
+              <>
+                <Todoitem>{item.todo}</Todoitem>
+                <button
+                  onClick={() => {
+                    setUpdateIdx(idx);
+                    setIsUpdate(true);
+                  }}
+                >
+                  수정
+                </button>
+                <button onClick={() => handleDeleteTodo(item.id, idx)}>
+                  삭제
+                </button>
+              </>
+            )}
+          </li>
+        ))}
+      </ol>
+      <button onClick={() => { localStorage.removeItem('access_token'); navigate('/') }}>로그아웃</button>
+    </Container>
   );
 }
 
